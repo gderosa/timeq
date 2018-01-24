6048f5ce15ce524b68e74f31bc94180a405576e0
Guido De Rosa <guidoderosa@gmail.com>
2018-01-24 18:21:06 +0000

    unitary decomp: wip

--- comp/qubiter/cs-decomposition.py
+++ comp/qubiter/cs-decomposition.py
@@ -26,9 +26,23 @@ init_unitary_mat = np.array([
   [-1,0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 1, 0, 0, 0, 0],
   [0, 0,-1, 0, 0, 0, 0, 0],
-  [0, 0,-1, 0, 0, 0, 0, 0],
-  [0, 0, 0,-1, 0, 0, 0, 0]
+  [0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0,-1, 0, 0, 0]
 ], dtype=np.complex_)
+
+'''
+init_unitary_mat = np.array([
+  [1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1]
+], dtype=np.complex_)
+'''
+
 
 emb = CktEmbedder(num_bits, num_bits)
 file_prefix = os.path.dirname(__file__) + '/io_folder/csd_test'
@@ -36,47 +50,28 @@ t = Tree(True, file_prefix, emb, init_unitary_mat, verbose=False)
 t.close_files()
 
 """
-The above code automatically creates an expansion of $U$ into DIAG and MP_Y lines. Next we print the Picture file that was created.
+The above code automatically creates an expansion of $U$ into DIAG and MP_Y lines.
 """
 
-file = file_prefix + '_3_ZLpic.txt'
-df = pd.read_csv(file, delim_whitespace=True, header=None)
-print(df)
+exit(0)
 
-file = file_prefix + '_3_eng.txt'
-df = pd.read_csv(file, delim_whitespace=True, header=None)
-print(df)
-
-#file_prefix = "io_folder/d_unitary_exact_check"
-num_bits = 4
+# Further decomposition inso real qu-gates
 num_angles = (1 << num_bits)
 emb = CktEmbedder(num_bits, num_bits)
 rad_angles = list(np.random.rand(num_angles)*2*np.pi)
 wr = DiagUnitarySEO_writer(file_prefix, emb, 'exact', rad_angles)
 wr.write()
 wr.close_files()
-file = file_prefix + '_4_ZLpic.txt'
+file = file_prefix + '_3_ZLpic.txt'
 with open(file) as f:
     print(f.read())
 
 """
-
-  |   |   |   Ph  
-  |   |   |   Rz  
-  |   |   @---X   
-  |   |   |   Rz  
-  |   |   @---X   
-  |   |   Rz  |   
-  |   @---X   |   
-
-Etc. etc.
-
 We can check that our exact expansion is correct as follows.
 We can multiply the gates of the expansion using the class SEO_MatrixProduct.
 Call the gate product matpro.prod_arr. Using the angles rad_angles that we stored,
 we can construct the exact diagonal unitary, call it exact_mat.
 Call err the norm of matpro.prod_arr - exact_mat, and print err.
-
 """
 
 matpro = SEO_MatrixProduct(file_prefix, num_bits)
@@ -85,45 +80,23 @@ err = np.linalg.norm(matpro.prod_arr - exact_mat)
 print("diag unitary error=", err)
 
 """
-
-diag unitary error= 5.48660383707e-12
-
 Next, we create English and Picture files containing an expansion of the 4 qubit gate
   Ry--%---%---%
 This represents a multiplexor matrix. The angles are chosen at random and stored in the variable rad_angles. We then print the Picture file.
 
 """
 
-#file_prefix = "io_folder/plexor_exact_check"
-num_bits = 4
 num_angles = (1 << (num_bits-1))
 emb = CktEmbedder(num_bits, num_bits)
 rad_angles = list(np.random.rand(num_angles)*2*np.pi)
 wr = MultiplexorSEO_writer(file_prefix, emb, 'exact', rad_angles)
 wr.write()
 wr.close_files()
-file = file_prefix + '_4_ZLpic.txt'
+file = file_prefix + '_3_ZLpic.txt'
 with open(file) as f:
     print(f.read())
 
 """
-  Ry  |   |   |   
-  X---+---+---@   
-  Ry  |   |   |   
-  X---+---@   |   
-  Ry  |   |   |   
-  X---+---+---@   
-  Ry  |   |   |   
-  X---@   |   |   
-  Ry  |   |   |   
-  X---+---+---@   
-  Ry  |   |   |   
-  X---+---@   |   
-  Ry  |   |   |   
-  X---+---+---@   
-  Ry  |   |   |   
-  X---@   |   |   
-
 We can check that our exact expansion is correct as follows.
 We can multiply the gates of the expansion using the class SEO_MatrixProduct.
 Call the gate product matpro.prod_arr.
@@ -131,16 +104,10 @@ Using the angles rad_angles that we stored, we can construct the exact multiplex
 call it exact_mat. Call err the norm of matpro.prod_arr - exact_mat, and print err.
 """
 
-
-
 matpro = SEO_MatrixProduct(file_prefix, num_bits)
 exact_mat = MultiplexorSEO_writer.mp_mat(rad_angles)
 err = np.linalg.norm(matpro.prod_arr - exact_mat)
 print("multiplexor error=", err)
-
-"""
-multiplexor error= 4.20573242068e-12
-"""
 
 """
 A moral of the above calculations is that using CSD quantum compiling blindly
