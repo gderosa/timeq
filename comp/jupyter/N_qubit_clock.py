9a5b09b7ace7e71887de754c715ec3170303bdd6
Guido De Rosa <gderosa@umail.ucc.ie>
2018-02-04 14:58:45 +0000

    .gitignore

--- comp/jupyter/N_qubit_clock.py
+++ comp/jupyter/N_qubit_clock.py
@@ -1,40 +1,15 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 from sympy import *
 from sympy.physics.matrices import mdft
 from sympy.physics.quantum.dagger import Dagger
 
-
-# In[2]:
-
+init_printing() 
 
 N = 4
 
-
-# In[3]:
-
-
 T = diag(*map(lambda k: 2*pi*k/N, range(N)))
-
-
-# In[4]:
-
 
 F = mdft(N)
 
-
-# In[5]:
-
-
 Omega = F * T * Dagger(F) * 2/pi
 
-
-# In[6]:
-
-
-Omega
-
+pprint(Omega)
