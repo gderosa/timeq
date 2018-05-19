9a98d3b4eddf4b43864fb792e7c04c3a16988b40
Guido De Rosa <gderosa@umail.ucc.ie>
2018-05-20 00:15:34 +0100

    sympy: J

--- comp/sympy/mppihalf.py
+++ comp/sympy/mppihalf.py
@@ -6,6 +6,7 @@ from sympy import *
 from sympy.physics.matrices import mdft
 from sympy.abc import omega
 from sympy.physics.quantum.constants import hbar
+from sympy.physics.quantum import TensorProduct
 
 init_printing(use_unicode=True)
 
@@ -16,8 +17,13 @@ T = (1/omega) * Matrix([
     [0, +pi/4]
 ])
 
-Omega = (4*omega**2/pi)*F*T*F.adjoint()
+Omega = (4*omega**2/pi)*F*T*(F.adjoint())
 
+Hs = I*omega*hbar*Matrix([
+    [0, 1],
+    [-1,0]
+])
 
+J = TensorProduct(hbar*Omega, eye(2)) + TensorProduct(eye(2), Hs)
 
-pprint(hbar * Omega)
+pprint(J)
