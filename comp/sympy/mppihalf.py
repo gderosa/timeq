d89dbe95c08015983ce35b4d90b7072dc0c6d14c
Guido De Rosa <gderosa@umail.ucc.ie>
2018-05-20 00:00:52 +0100

    Omega in py

--- comp/sympy/mppihalf.py
+++ comp/sympy/mppihalf.py
@@ -1,6 +1,23 @@
-# from IPython.lib.pretty import pretty
+'''
+PW 1=1 qubit
+'''
 
 from sympy import *
 from sympy.physics.matrices import mdft
+from sympy.abc import omega
+from sympy.physics.quantum.constants import hbar
 
-print(repr(mdft(2)))
+init_printing(use_unicode=True)
+
+F = mdft(2)
+
+T = (1/omega) * Matrix([
+    [-pi/4, 0],
+    [0, +pi/4]
+])
+
+Omega = (4*omega**2/pi)*F*T*F.adjoint()
+
+
+
+pprint(hbar * Omega)
