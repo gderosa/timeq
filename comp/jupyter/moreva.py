738fffb498171b210fcd5b07679d49c90e9a7cc5
Guido De Rosa <gderosa@umail.ucc.ie>
2023-01-08 16:10:40 +0000

    t repr: progress...

--- comp/jupyter/moreva.py
+++ comp/jupyter/moreva.py
@@ -1,6 +1,16 @@
 from sympy import *
+from sympy.matrices.expressions.fourier import DFT
+from sympy.physics.quantum import TensorProduct
+from sympy.physics.quantum.dagger import Dagger
+
 
 init_printing()
+
+F = DFT(2).as_explicit()
+
+F
+
+F @ Dagger(F)
 
 HT_hv = Matrix([
     [ 0, I],
@@ -13,5 +23,19 @@ HS_hv = Matrix([
 ])
 
 eigensys = HT_hv.eigenvects()
+
+eigensys
+
+U = Matrix([eigensys[0][2][0].T, eigensys[1][2][0].T]).T / sqrt(2)
+
+U
+
+ Dagger(U) @ U
+
+Dagger(U) @ HS_hv @ U
+
+UU = TensorProduct(F@U, eye(2))
+
+UU
 
 
