90925d46d0c9823519b1889e4ddbd52d17934026
Guido De Rosa <gderosa@umail.ucc.ie>
2023-01-10 07:14:27 +0000

    more explaination on polarization-basis clock

--- comp/jupyter/moreva+diag_clock_pol.py
+++ comp/jupyter/moreva+diag_clock_pol.py
@@ -60,19 +60,58 @@ T_HT = (pi / (delta_HT**2)) * F @ HT_HT @ Dagger(F)
 
 T_HT
 
-# interestingly, it is diagonal, but in reverse time order...
+# interestingly, it is diagonal in the polarization basis (though in reverse time order)...
 T_hv = U @ T_HT @ Dagger(U)
 T_hv
 
+# `T_hv` represents $\hat{T}$ in the basis $| H \rangle, | V \rangle$, therefore:
+#
+# \begin{align}
+#   \left._{T}\left\langle{T; \frac{\pi}{4}}\right|\right. &=
+#   \left._{T}\left\langle{H}\right|\right.
+#   \\
+#   \left._{T}\left\langle{T; -\frac{\pi}{4}}\right|\right. &=
+#   \left._{T}\left\langle{V}\right|\right.
+# \end{align}
+#
+# We can set:
+# $$
+#   t_i = -\frac{\pi}{4} ; \quad
+#   t_f =  \frac{\pi}{4} ; \quad
+#   \delta{T} = t_f - t_i = \frac{\pi}{2} .
+# $$
+#
+#
+# Hence, using the definition of `Psi_hv`:
+#
+# \begin{align}
+#   \left|\psi_i\right\rangle :=
+#   \left|\psi_i\right\rangle_{PW} &:=
+#   \left|\psi_i\right\rangle_S &= 
+#   \left._{T}\left\langle{T; t_i}\middle|\Psi\right\rangle\right\rangle &=
+#   \left._{T}\left\langle{V}\middle|\Psi\right\rangle\right\rangle &=
+#   -\left|H\right\rangle_S 
+#   \\
+#   \left|\psi_f\right\rangle_{PW} &:=
+#   \left|\psi_f\right\rangle_S &= 
+#   \left._{T}\left\langle{T; t_f}\middle|\Psi\right\rangle\right\rangle &= 
+#   \left._{T}\left\langle{H}\middle|\Psi\right\rangle\right\rangle &=
+#   \; \left|V\right\rangle_S
+#   \;.
+# \end{align}
+#
+# If a qubit at time $t_i$ is in state $\left|\psi_i\right\rangle$,
+# we want to check that its standard time-evolved state $\left|\psi_f\right\rangle_{\text{Schrod}}$
+# at time $t_f$
+# coincides with the $\left|\psi_f\right\rangle_{PW}$ above
+# (which is obtained projecting the Page&ndash;Wooters history-vector $|\Psi\rangle\rangle$ on a time eigenbasis).
+
 psi_i = -H_hv
+psi_i
 
 psi_f_PW = V_hv
 
 delta_T = pi/2
-
-
-
-
 
 
 
